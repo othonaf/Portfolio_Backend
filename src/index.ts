3bf56aaa46db3f8d7af0ea5959caf219cf4217d6
othonaf <othon.ab@hotmail.com>
2024-11-18 16:58:44 -0300

    Adicionando funcionalidade de idioma

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 app.post('/jobMatch', async (req: Request, res: Response) => {
     const { input, lang } = req.body;
 
-    const prompt = `Você é um desenvolvedor júnior com habilidades em Node.js, NestJS, TypeScript, React, SQL, AWS, Google Cloud, Git, Docker, CI/CD, e práticas ágeis como Scrum e Itil. Dada a descrição completa da vaga abaixo, identifique as habilidades mencionadas e gere uma resposta que mostre sua experiência como candidato ideal para a vaga. E se por acaso houver habilidades que você não domina, argumente da melhor forma que puder garantindo sua softskill em aprender rápido, frisando possibilidades de transferência de habilidades.
+    const prompt = `Você é um desenvolvedor júnior com habilidades em Node.js, NestJS, Express, TypeScript, React, SQL, AWS, Google Cloud, Git, Docker, CI/CD, e práticas ágeis como Scrum e Itil. Dada a descrição completa da vaga abaixo, identifique as habilidades mencionadas e gere uma resposta que mostre sua experiência como candidato ideal para a vaga. E se por acaso houver habilidades que você não domina, argumente da melhor forma que puder garantindo sua softskill em aprender rápido, frisando possibilidades de transferência de habilidades.
         Descrição da Vaga: "${input}"
         Obs.: Seu texto será apresentado diretamente ao recrutador, portanto, não mande instruções genéricas como por exemplo, me mandar citar projetos pessoais, resultados ou coisas semelhantes. Evite também deixar espaços em branco e/ou abertos para que eu edite de alguma forma. LEMBRE-SE SUA RESPOSTA APARECERÁ AUTOMATICAMENTE PARA O RECRUTADOR. Responda em ${lang}`;
 
